c1c3bd4fcb7cd1bcc8a39b3cc3a3be28165ee1b8
Iram Qureshi <140047835+Iram-Qureshi@users.noreply.github.com>
2024-02-01 21:37:45 +0530

    Add files via upload

---
+++ Linear_Search.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a[100];
+    int size ,num;
+    cout<<"Enter the size of Array: "<<endl;
+    cin>>size;
+    cout<<"Enter the Element of Array: "<<endl;
+    for(int i=0; i<size; i++)
+    {
+        cin>>a[i];
+    }
+    cout<<"Enter the Element to Search: "<<endl;
+    cin>>num;
+    for(int j=0; j<size; j++)
+    {
+        if(a[j]== num)
+        {
+            cout<<"Element Found at "<<j<<" indexing "<<endl;
+        }
+    }
+}
