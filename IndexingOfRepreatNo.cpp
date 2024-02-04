04d354abe05e19dad3c82e60cdcd8fa16a0ca2fb
Iram Qureshi <140047835+Iram-Qureshi@users.noreply.github.com>
2024-02-04 20:27:30 +0530

    Add files via upload
    Indexing of Reapeated Number in C++ Program

---
+++ IndexingOfRepreatNo.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int a[100];
+    int s, n;
+    cout<<"Enter size of array: "<<endl;
+    cin>>s;
+    cout<<"Enter "<<s<<" Elements in Array: "<<endl;
+    for(int i=0;i<s;i++)
+    {
+        cin>>a[i];
+    }
+    cout<<"Enter no. who's repetation will find: "<<endl;
+    cin>>n;
+    int itr=0;
+    cout<<"Element "<<n<<" Present at Indexing ";
+    for(int j=0; j<s;j++)
+    {
+        if(n==a[j])
+        {
+            cout<<" "<<j<<" ";
+            itr++;
+        }
+    }
+    cout<<"\n";
+    cout<<n<<" Repeat "<<itr<<" Times"<<endl;
+    return 0;
+}
